d141d44b6b6731f10cb06893197f9f80db39893a
Jun <mafer.jun@outlook.com>
2015-11-06 00:04:36 +0800

    viewedit

--- app/core/IndexView/views.py
+++ app/core/IndexView/views.py
@@ -3,3 +3,7 @@ from . import index, render_template
 @index.route('/')
 def index():
     return render_template('indexs/base.html')
+
+@index.route('/index')
+def indexpage():
+    return render_template('indexs/base.html')
