b80a3ab3abc1ce1424c234a300fe9492705bb7a0
Jun <mafer.jun@outlook.com>
2015-11-08 14:07:54 +0800

    indexs view added

--- app/core/IndexView/views.py
+++ app/core/IndexView/views.py
@@ -2,8 +2,9 @@ from . import index, render_template
 
 @index.route('/')
 def index():
-    return render_template('indexs/base.html')
+    return render_template('indexs/topic-content.html')
 
-@index.route('/index')
-def indexpage():
-    return render_template('indexs/base.html')
+
+#@index.route('/indexss')
+#def indexpage():
+#    return render_template('indexs/base.html')
